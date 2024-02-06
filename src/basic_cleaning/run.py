50e821ca62e7d356e5ab17e60d5a2510157a8486
David Blackwelder <github@davidblackwelder.com>
2024-02-06 04:17:59 -0500

    Fix failed test

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,7 +34,10 @@ def go(args):
     idx = df['price'].between(float(args.min_price), float(args.max_price))
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
-    # TODO: add code to fix the issue happened when testing the model
+
+    # This will drop rows in the dataset that are not in the proper geolocation.
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     # Save the cleaned data
     logger.info('Saving and exporting cleaned data.')
