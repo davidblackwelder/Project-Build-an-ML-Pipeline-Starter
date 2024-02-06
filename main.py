84bd7d342e7f73824a5b304a5d682b1e411b0cbd
David Blackwelder <github@davidblackwelder.com>
2024-02-05 19:29:51 -0500

    Fix formatting

--- main.py
+++ main.py
@@ -79,15 +79,14 @@ def go(config: DictConfig):
 
         if "data_split" in active_steps:
             _ = mlflow.run(
-                f"{config['main']['components_repository']
-                   }/train_val_test_split",
+                f"{config['main']['components_repository']}/train_val_test_split",
                 'main',
                 parameters={
                     "input": "clean_sample.csv:latest",
                     "test_size": config['modeling']['test_size'],
                     "random_seed": config['modeling']['random_seed'],
                     "stratify_by": config['modeling']['stratigy_by']
-                }
+                },
             )
 
         if "train_random_forest" in active_steps:
